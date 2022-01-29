c2dc035b3aee20bb59e2bc2aecd922241a0d508f
ihassansattar <i.hassansattar@gmail.com>
2022-01-29 15:25:29 +0500

    Update the body color changing on scroll

--- javsScript.js
+++ javsScript.js
@@ -6,8 +6,7 @@ $(window).scroll(function() {
       $panel = $('.panel');
   
   // Change 33% earlier than scroll position so colour is there when you arrive.
-  var scroll = $window.scrollTop() + ($window.height() / 3);
- 
+  var scroll = $window.scrollTop() + ($window.height() / 1.4); 
   $panel.each(function () {
     var $this = $(this);
     
